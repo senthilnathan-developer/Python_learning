85c4783b3f44e9f6c633915392666e6a661449ca
senthilnathan-developer <eswarensenthil@gmail.com>
2026-07-16 16:41:32 +0530

    Python Learing

--- basic-problem/sum12.py
+++ basic-problem/sum12.py
@@ -2,5 +2,5 @@ def add_even_checker(a):
     if a%2==0:
         print(a,"is a even number")
     else:
-        print(a, "is a not even nunmber")
+        print(a, "is a not even number")
 add_even_checker(19)
---
+++ basic-problem/sum26.py
@@ -0,0 +1,7 @@
+value = input("Enter a Traffic color: ")
+if value == "Red":
+    print("Stop")
+elif value == "Yellow":
+    print("Get Ready")
+elif value == "Green":
+    print("Go")
