b44a1911a93837e98f1e73f58180951344dcb8f2
senthilnathan-developer <eswarensenthil@gmail.com>
2026-07-17 19:34:47 +0530

    Problem solving

---
+++ basic-problem/sum29.py
@@ -0,0 +1,4 @@
+num = 10
+while num > 0:
+    print(num,"x 1 =",num)
+    num -= 1
---
+++ basic-problem/sum30.py
@@ -0,0 +1,7 @@
+num1 = 9
+num2 = 2
+num3 = num1 +num2
+if num3  % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
